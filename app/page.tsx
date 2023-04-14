3b6f5b34d005c12204441a89eaca60e175bfca3f
bilal-faisal <bilalfaisal.developer@gmail.com>
2023-04-14 18:06:08 +0500

    Integrated Rainbow Wallet

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import Wallet from "./Components/Wallet";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="flex flex-col items-center gap-6">
+      <h1 className="text-2xl">Panaverse Wallet Assignment</h1>
+      <Wallet />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
